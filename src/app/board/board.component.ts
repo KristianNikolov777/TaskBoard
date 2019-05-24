9b6b02ff99b76a18ab71a3ab2d60bff004f8cb3e
Kristian Nikolov <kristiangeorgiev777@gmail.com>
2019-05-24 23:11:16 +0300

    Added mocked list data.

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { List } from '../shared/list.model';
+
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
+  lists: List[] = [
+    new List('To Do', []),
+    new List('Doing', []),
+    new List('Done', []),
+    new List('sfsd', []),
+    new List('sfsd', []),
+  ];
 
   constructor() { }
 
--- src/app/board/list/list.component.ts
+++ src/app/board/list/list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { List } from '../../shared/list.model';
 
 @Component({
   selector: 'app-list',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  @Input() list: List;
 
   constructor() { }
 
